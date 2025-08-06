b2ee2c989592bf5be5de8fe4f3af26715475e277
Oytun Tez <oytun@motaword.com>
2025-08-06 19:14:07 -0400

    draft branding changes for brainyes

--- components/site-brand.tsx
+++ components/site-brand.tsx
@@ -1,26 +1,54 @@
+'use client'
+
 import * as React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useTheme } from 'next-themes'
 import { cn } from '@/lib/utils'
 
 interface SiteBrandProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const SiteBrand = ({ className, ...props }: SiteBrandProps) => {
+  const { theme } = useTheme()
+  const [mounted, setMounted] = React.useState(false)
+
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+
   return (
     <Link href="/" className={cn('flex items-center gap-2', className)}>
       <div className="relative">
-        <Image
-          src="/assets/images/main/tracts2.png"
-          alt="Cerebro"
-          width={80}
-          height={55}
-          className="object-cover mt-1"
-          priority
-        />
+        {!mounted ? (
+          <Image
+            src="/assets/images/main/logo.png"
+            alt="Brain Yes"
+            width={200}
+            height={75}
+            className="object-cover mt-1"
+            priority
+          />
+        ) : (
+          <>
+            <Image
+              src="/assets/images/main/logo.png"
+              alt="Brain Yes"
+              width={200}
+              height={75}
+              className="object-cover mt-1 dark:hidden"
+              priority
+            />
+            <Image
+              src="/assets/images/main/logo-dark.png"
+              alt="Brain Yes"
+              width={200}
+              height={75}
+              className="hidden object-cover mt-1 dark:block"
+              priority
+            />
+          </>
+        )}
       </div>
-      <span className="font-['Noto_Sans_KR'] text-xl font-semibold text-black">
-        Cerebro
-      </span>
     </Link>
   )
 }
